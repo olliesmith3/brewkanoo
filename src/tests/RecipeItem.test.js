0378bb126df7f420aa67113e330891fdd20abe7a
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-11-13 14:05:31 +0000

    test for RecipeItem now working

--- src/tests/RecipeItem.test.js
+++ src/tests/RecipeItem.test.js
@@ -4,12 +4,26 @@ import RecipeItem from '../components/RecipeItem.js';
 
 
 it("should show a clickable hide button after a recipe is clicked", () => {
-    const mockHideButton = ("Hide")
-    render(<RecipeItem recipeProp={mockHideButton}/>)
+    const mockRecipe = {
+        id: 1,
+        title: "Ollie's Big Flavor Non-Descript Homebrew",
+        walkthrough: [
+          { description: "Boil the water", time: 10},
+        ],
+        ingredients: {
+          grains: ["chocolate malt", "crystal malt"],
+          hops: ["Bramling Cross", "Citra"],
+          yeast: "Safeale",
+        },
+        equipment: ["A bucket","Thermometer"]
+      }
+    render(<RecipeItem recipeProp={mockRecipe}/>)
+
     const button = screen.getByRole('button');
     button.click();
-    const hideButton = screen.getAllByRole('button');
-    expect(hideButton).toHaveLength(1);
+    screen.debug();
+    const buttons = screen.getAllByRole('button');
+    expect(buttons).toHaveLength(4);
 
 
 });
